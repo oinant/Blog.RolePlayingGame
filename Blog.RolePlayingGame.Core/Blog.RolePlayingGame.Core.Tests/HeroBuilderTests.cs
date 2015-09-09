681769dd1373cf09a9800140e2f07a5d485e04c0
oinant <antoine.sauvinet+github@gmail.com>
2015-09-09 14:51:16 +0200

    initial commit

---
+++ Blog.RolePlayingGame.Core/Blog.RolePlayingGame.Core.Tests/HeroBuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using Blog.RolePlayingGame.Core;
+using Shouldly;
+
+
+namespace Blog.RolePlayingGame.Core.Tests
+{
+    public class HeroBuilderTests
+    {
+        public void An_HeroBuilder_can_build_a_warrior()
+        {
+            Hero expected =  new HeroBuilder()
+                                 .OfWarriorClass()
+                                 .Create();
+
+            expected.GetClass().ShouldBe(HeroClass.Warrior);
+        }
+
+        public void WhenBuilt_a_level1_hero_has_10_health()
+        {
+            Hero expected = new HeroBuilder()
+                                 .WithLevel(1)
+                                 .Create();
+
+            expected.GetHealth().ShouldBe(10);
+        }
+
+        public void WhenBuilt_a_level2_hero_has_20_health()
+        {
+            Hero expected = new HeroBuilder()
+                                 .WithLevel(2)
+                                 .Create();
+
+            expected.GetHealth().ShouldBe(20);
+        }
+    }
+}
---
+++ Blog.RolePlayingGame.Core/Blog.RolePlayingGame.Core/Hero.cs
@@ -0,0 +1,91 @@
+﻿namespace Blog.RolePlayingGame.Core
+{
+    public interface ITarget
+    {
+        void ReceivePhysicalAttack(int strength);
+        void ReceiveMagicalAttack(int strength);
+    }
+
+    public class Hero
+    {
+        private HeroClass _class;
+        private int _level;
+        private int _health;
+        private int _strenght;
+        private int _spirit;
+        private int _speed;
+
+        public Hero(HeroClass @class, int level, int health, int strenght, int spirit, int speed)
+        {
+            _speed = speed;
+            _spirit = spirit;
+            _strenght = strenght;
+            _health = health;
+            _level = level;
+            _class = @class;
+        }
+
+        public void Hit(ITarget target)
+        {
+            target.ReceivePhysicalAttack(this._strenght);
+        }
+
+        public void Spell(ITarget target)
+        {
+            target.ReceiveMagicalAttack(this._spirit);
+        }
+
+        public HeroClass GetClass()
+        {
+            return _class;
+        }
+
+        public object GetHealth()
+        {
+            return _health;
+        }
+    }
+
+    public enum HeroClass
+    {
+        Warrior = 1,
+        Wizard = 2,
+        Thief = 3
+    }
+
+    public class HeroBuilder
+    {
+        private HeroClass _class;
+        private int _level;
+        private int _health;
+
+        public HeroBuilder()
+        {
+            _level = 1;
+        }
+
+        public HeroBuilder OfWarriorClass()
+        {
+            this._class = HeroClass.Warrior;
+            return this;
+        }
+
+        public HeroBuilder WithLevel(int level)
+        {
+            this._level = level;
+            this._health = level*10;
+            return this;
+        }
+
+        public Hero Create()
+        {
+            return new Hero(@class: _class, 
+                            level:1, 
+                            health: _health,
+                            strenght: 0,
+                            spirit: 0,
+                            speed: 0);
+        }
+
+    }
+}
